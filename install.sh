42e5b4167fcf71fc6904908200a81448f8a4c2ff
hadrori <hadrori.hs@gmail.com>
2016-08-15 18:08:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,34 +9,17 @@ green='\033[0;32m'
 bold='\033[1m'
 restore_color='\033[0m\033[22m'
 
-if [ -e ~/.bash_profile ]; then
-  echo -e "${red}${bold}[ERROR]:${restore_color} .bash_profile already exists"
-  echo -e "${red}${bold}[ERROR]:${restore_color} You should merge files by hand."
-else
-  ln -s ./.bash_profile ~/.bash_profile
-  echo -e "${green}${bold}[OK]:${restore_color} .bash_profile has linked"
-fi
+link_file() {
+  if [ -e ~/$1 ]; then
+    echo -e "${red}${bold}[ERROR]:${restore_color} $1 already exists"
+    echo -e "${red}${bold}[ERROR]:${restore_color} You should merge files by hand."
+  else
+    ln -s $(echo $(pwd)/$1) ~/$1
+    echo -e "${green}${bold}[OK]:${restore_color} $1 has linked"
+  fi  
+}
 
-if [ -e ~/.tmux.conf ]; then
-  echo -e "${red}${bold}[ERROR]:${restore_color} .tmux.conf already exists"
-  echo -e "${red}${bold}[ERROR]:${restore_color} You should merge files by hand."    
-else
-  ln -s $(echo $(pwd)/.tmux.conf) ~/.tmux.conf
-  echo -e "${green}${bold}[OK]:${restore_color} .tmux.conf has linked"
-fi
-
-if [ -e ~/.zshrc ]; then
-  echo -e "${red}${bold}[ERROR]:${restore_color} .zshrc already exists"
-  echo -e "${red}${bold}[ERROR]:${restore_color} You should merge files by hand."    
-else
-  ln -s $(echo $(pwd)/.zshrc) ~/.zshrc
-  echo -e "${green}${bold}[OK]:${restore_color} .zshrc has linked"
-fi
-
-if [ -e ~/.gitconfig ]; then
-  echo -e "${red}${bold}[ERROR]:${restore_color} .gitconfig already exists"
-  echo -e "${red}${bold}[ERROR]:${restore_color} You should merge files by hand."    
-else
-  ln -s $(echo $(pwd)/.gitconfig) ~/.gitconfig
-  echo -e "${green}${bold}[OK]:${restore_color} .gitconfig has linked"
-fi
+link_file .bash_profile
+link_file .zshrc
+link_file .tmux.conf
+link_file .gitconfig
