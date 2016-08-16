89bcd29b6c10ec2dad528573e8d4298c5ffe3d57
hadrori <hadrori.hs@gmail.com>
2016-08-16 16:47:26 +0900

    Update installer

--- install.sh
+++ install.sh
@@ -9,11 +9,15 @@ green='\033[0;32m'
 bold='\033[1m'
 restore_color='\033[0m\033[22m'
 
+if [ -z "$DEST_DIR" ]; then
+  export DEST_DIR="$HOME/.dotfiles"
+fi
+
 link_file() {
   if [ -e ~/$1 ]; then
     echo -e "${red}${bold}[ERROR]:${restore_color} $1 already exists"
   else
-    ln -s $(pwd)/$1 ~/$1
+    ln -s $DEST_DIR/$1 ~/$1
     echo -e "${green}${bold}[OK]:${restore_color} $1 has linked"
   fi
 }
