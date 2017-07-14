30af3747470dd619d140cb265e77dd7a796b81d4
kisozuka <kisozuka@indeed.com>
2017-07-14 10:42:22 -0500

    Fixed color

--- .zsh-theme.sh
+++ .zsh-theme.sh
@@ -9,7 +9,7 @@ zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' formats '%F{075}(%F{078}%b%F{214}%u%c%{$reset_color%}%F{075})%{$reset_color%}'
 zstyle ':vcs_info:*' actionformats '%R/(%a |%m )'
 zstyle ':vcs_info:*' unstagedstr '%F{214}*%{$reset_color%}'
-zstyle ':vcs_info:*' stagedstr '%F{214}+%{$reset_color%}'
+zstyle ':vcs_info:*' stagedstr '%F{green}*%{$reset_color%}'
 
 autoload -Uz vcs_info
 setopt prompt_subst
@@ -32,7 +32,7 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 function +vi-git-untracked() {
     if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]]; then
         if git status --porcelain | grep '??' &> /dev/null ; then
-            hook_com[staged]+='+'
+            hook_com[staged]+='%F{214}+%{$reset_color%'}
         fi
     fi
 }
